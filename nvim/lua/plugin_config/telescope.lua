e8b601bd1d2f537a5b58489c0dadff7d79b261e1
Eric Zheng <ezheng118@gmail.com>
2025-06-11 11:10:39 -0400

    change telescope buffer list behavior

--- nvim/lua/plugin_config/telescope.lua
+++ nvim/lua/plugin_config/telescope.lua
@@ -13,5 +13,5 @@ local builtin = require("telescope.builtin")
 
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {desc="[f]ind [f]iles"})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {desc="[f]ind by [g]rep"})
-vim.keymap.set("n", "<leader>lb", builtin.buffers, {desc="[l]ist [b]uffers"})
+vim.keymap.set("n", "<leader>lb", function() builtin.buffers({sort_mru=true, ignore_current_buffer=true}) end, {desc="[l]ist [b]uffers"})
 
